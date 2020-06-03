f66a7bf713bd43b38a2c9b241c55b1fbf78f118f
SpiritReaver <54217513+SpiritReaver@users.noreply.github.com>
2020-06-03 00:09:21 -0500

    Update Competitor.java

--- src/main/java/com/example/models/Competitor.java
+++ src/main/java/com/example/models/Competitor.java
@@ -1,71 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.example.models;
 
 import com.sun.istack.NotNull;
 import java.io.Serializable;
 import java.util.Calendar;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
+
 import javax.persistence.PreUpdate;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
+import org.eclipse.persistence.nosql.annotations.DataFormatType;
+import org.eclipse.persistence.nosql.annotations.Field;
+import org.eclipse.persistence.nosql.annotations.NoSql;
 
 /**
- *
- * @author Mauricio
+ * ** @author Mauricio
  */
+@NoSql(dataFormat = DataFormatType.MAPPED)
 @Entity
 @XmlRootElement
-public class Competitor implements Serializable{
-     private static final long serialVersionUID = 1L;
-     
+public class Competitor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    
+    @GeneratedValue
+    @Field(name = "_id")
+    private String id;
     @NotNull
     @Column(name = "create_at", updatable = false)
     @Temporal(TemporalType.DATE)
     private Calendar createdAt;
-
     @NotNull
     @Column(name = "updated_at")
     @Temporal(TemporalType.DATE)
     private Calendar updatedAt;
-    
     private String name;
-    
     private String surname;
-    
     private int age;
-    
     private String telephone;
-    
     private String cellphone;
-    
     private String address;
-    
     private String city;
-    
     private String country;
-    
     private boolean winner;
-    //
-    
-    public Competitor(){
-        
+
+    @Embedded
+    private Vehicle vehicle;
+    @Embedded
+    private Producto product;
+
+    public Competitor() {
     }
-    
+
     @PreUpdate
     private void updateTimestamp() {
         this.updatedAt = Calendar.getInstance();
@@ -76,11 +68,11 @@ public class Competitor implements Serializable{
         this.createdAt = this.updatedAt = Calendar.getInstance();
     }
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -155,5 +147,23 @@ public class Competitor implements Serializable{
     public void setWinner(boolean winner) {
         this.winner = winner;
     }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Producto getProduct() {
+        return product;
+    }
+
+    public void setProduct(Producto product) {
+        this.product = product;
+    }
+    
+    
     
 }
